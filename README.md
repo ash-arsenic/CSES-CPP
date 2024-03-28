2c76ddab669108b6d6cc23db5ce3fa6e56ee36d1
Ash Arsenic <ashchauhan498@gmail.com>
2024-03-28 10:16:56 +0530

    Adding Demo code

---
+++ README.md
@@ -0,0 +1,1 @@
+"Each problem is solved on seperate branch, Please refer to problem title for branch name" 
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello CSES!!" << endl;
+    return 0;
+}
