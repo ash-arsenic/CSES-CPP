903c34904e05ddee105a22d374cc8a3d9e9dc1ec
Ash Arsenic <ashchauhan498@gmail.com>
2024-03-28 11:42:31 +0530

    Permutations solved

--- main.cpp
+++ main.cpp
@@ -4,30 +4,23 @@ using namespace std;
 
 typedef long long ll;
 
-class IncreasingArray {
+class Permutations {
     public:
     static void execute() {
         int input1;
         cin >> input1;
-
-        int input2[input1];
-        for(int i=0; i<input1; i++) cin >> input2[i];
-
-        long result = 0;
-        for(int i=1; i<input1; i++) {
-            int diff = input2[i-1] - input2[i]; // Get the difference
-            if(diff > 0) {
-                input2[i] += diff; // Update the current element
-                result += diff; // Add all the difference
-            }
+        if((input1 == 3) || (input1 == 2)) { // Edge case
+            cout << "NO SOLUTION";
+            return;
         }
 
-        cout << result;
+        for(int i=2; i<=input1; i+=2) cout << i << " "; // Print all even number in ascending order.
+        for(int i=1; i<=input1; i+=2) cout << i << " "; // Print all odd number in ascending order.
     }
 };
 
 int main()
 {
-    IncreasingArray::execute();
+    Permutations::execute();
     return 0;
 }
