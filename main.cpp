756142ffcdc2fed0b2ca13bcc846f0d3b1d279ad
Ash Arsenic <ashchauhan498@gmail.com>
2024-03-28 10:32:45 +0530

    Weird Algorithm Solved

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
+class WierdAlgorithm {
+    public:
+    static void execute() {
+        long input;
+        cin >> input;
+        while (input > 1) {
+            cout << input << " ";
+            input = input % 2 == 0 ? input/2 : input*3+1;
+        }
+        cout << 1;
+    }
+};
+
 int main()
 {
-    cout << "Hello CSES!!" << endl;
+    WierdAlgorithm::execute();
     return 0;
 }
