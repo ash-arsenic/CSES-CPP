fd7a22d5fd4b01804791776997ed831911a461df
Ash Arsenic <ashchauhan498@gmail.com>
2024-03-28 10:44:17 +0530

    Missing number solved

--- main.cpp
+++ main.cpp
@@ -2,21 +2,29 @@
 
 using namespace std;
 
-class WierdAlgorithm {
+typedef long long ll;
+
+class MissingNumber {
     public:
     static void execute() {
         long input;
         cin >> input;
-        while (input > 1) {
-            cout << input << " ";
-            input = input % 2 == 0 ? input/2 : input*3+1;
-        }
-        cout << 1;
+        long input2[input-1];
+
+        for(int i=0; i<input-1; i++) cin >> input2[i];
+
+        ll total = 0;
+        for(int i=1; i<=input; i++) total += i; // Adding 1 to N numbers
+
+        ll inputTotal = 0;
+        for(int i=0; i<input-1; i++) inputTotal += input2[i]; // Adding all the input numbers
+
+        cout << total - inputTotal; // 15 - 11 -> 4
     }
 };
 
 int main()
 {
-    WierdAlgorithm::execute();
+    MissingNumber::execute();
     return 0;
 }
